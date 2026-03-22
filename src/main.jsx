5d44242c2f0a6d436be69bd647d31f2b74bd4376
Shaun Mathew <shaunmathew@gmail.com>
2026-03-22 15:48:22 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App' // Make sure there is no '/pages/' here
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
