9ca5ec320815f148904110ac44ddb2fd572566ac
ChristeenaJestin <christeenajestin7@gmail.com>
2026-04-19 10:18:37 +0530

    updated frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
   const handleLogin = async () => {
     try {
 
-      const res = await fetch("https://ecoloop-backend-ukxb.onrender.com/api/login", {
+      const res = await fetch("https://ecoloop-backend-ukxb.onrender.com/api/login/", {
         method: "POST", // ✅ comma fixed
         headers: {
           "Content-Type": "application/json",
